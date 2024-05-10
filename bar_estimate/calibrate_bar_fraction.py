f655c9ff3c6c10e65c0b672f4c88ad53d61af08e
metasophie <2296131461@qq.com>
2024-05-10 18:53:12 +0100

    update

--- bar_estimate/calibrate_bar_fraction.py
+++ bar_estimate/calibrate_bar_fraction.py
@@ -128,6 +128,9 @@ plt.savefig(f'bar_estimate/f_bar_M_all_filters.png')
 N_BINS_mag = 5
 bins_mag = [20, 21, 22, 23, 24, 25]
 
+N_BINS_Re = 4
+bins_Re = [0., 0.2, 0.4, 0.6, 0.8]
+
 
 result = pd.read_csv(f"bar_estimate/F200W_sampling.csv")
 result = result[(result['zfit_50'] <= 1.) & (result['logM_50'] > 9.) & (result['logM_50'] < 10.)]
@@ -145,23 +148,25 @@ cat_name = "CEERS_DR05_adversarial_asinh_4filters_1122_4class_ensemble_v02_stell
 cat = pd.read_csv(os.path.join(cat_dir,cat_name))
 mag_F200W = cat['F200W_MAG'].values
 
+Re_F200W = cat['F200W_RE'].values
+
 feature_thresholds = [0.2, 0.3, 0.4]
 bar_thresholds = [0.4, 0.5, 0.6]
 
 # z<1, binned by mag_F200W, varying p_bar threshold
-num_disks = np.zeros((3, N_BINS_mag, N_RUNS))
-num_barred_disks = np.zeros((3, N_BINS_mag, N_RUNS))
+num_disks = np.zeros((3, N_BINS_Re, N_RUNS))
+num_barred_disks = np.zeros((3, N_BINS_Re, N_RUNS))
 
 for k in range(3):
     for i in range(len(result)):
-        for j in range(N_BINS_mag):
-            if (mag_F200W[id[i]] >= bins_mag[j]) & (mag_F200W[id[i]] < bins_mag[j+1]):
+        for j in range(N_BINS_Re):
+            if (Re_F200W[id[i]] >= bins_Re[j]) & (Re_F200W[id[i]] < bins_Re[j+1]):
                 feature = str2array(feature_count[i])
                 edgeon = str2array(edgeon_count[i])
                 bar = str2array(bar_count[i])
 
                 # is_disk = (feature >= 0.3*N_VOLS) & (edgeon <= 0.5*feature) & (q[i] >= 0.5)
-                is_disk = (feature >= 0.3*N_VOLS) & (feature-edgeon >= 10) & (q[i] >= 0.5)
+                is_disk = (feature >= 0.3*N_VOLS) & (feature-edgeon >= 15) & (q[i] >= 0.5)
                 num_disks[k,j,:] += is_disk.astype(int)
 
                 is_barred_disk = is_disk & (bar >= bar_thresholds[k]*(feature-edgeon))
@@ -172,19 +177,19 @@ print(num_disks, flush=True)
 bar_fraction = num_barred_disks / num_disks
 err = np.sqrt(np.sum(bar_fraction*(1-bar_fraction)/num_disks, axis=2)/N_RUNS**2 + np.var(bar_fraction, axis=2))
 
-bin_centers = [(bins_mag[i] + bins_mag[i+1]) / 2 for i in range(N_BINS_mag)]
+bin_centers = [(bins_Re[i] + bins_Re[i+1]) / 2 for i in range(N_BINS_Re)]
 
 plt.figure()
 for k in range(3):
     plt.errorbar(bin_centers, np.mean(bar_fraction[k,:,:], axis=1), yerr=err[k,:], 
                     fmt='--s', capsize = 5, 
                     label=r'$p_\mathrm{bar}\geq%.1f$'%bar_thresholds[k])
-plt.xticks(bins_mag)
-plt.xlabel("Mag F200W")
+plt.xticks(bins_Re)
+plt.xlabel("Re F200W/arcsec")
 plt.ylabel("Bar fraction")
 plt.ylim((0, 1.))
 plt.legend(loc='upper right')
-plt.savefig(f'bar_estimate/f_bar_F200W_mag_p_bar.png')
+plt.savefig(f'bar_estimate/f_bar_F200W_Re_p_bar.png')
 
 
 # z<1, binned by mag_F200W, varying p_feature threshold
--- test/test_cat.py
+++ test/test_cat.py
@@ -23,15 +23,15 @@ col_names = ['RA_1','DEC_1']
 cols = cat.columns
 
 id = pd.read_csv("bar_estimate/F200W_sampling.csv")['id'].values
-mag = cat['F200W_MAG'].values[id]
-plt.hist(mag)
-plt.savefig('test_mag_F200W.png')
+mag = cat['F200W_RE'].values[id]
+plt.hist(mag, bins=[0., 0.2, 0.4, 0.6, 0.8])
+plt.savefig('test_re_F200W.png')
 
 for col in cols:
     print(col)
 
 # print(cla[['pixrad','radius_select','flux_rad_0p50','which_nircam','nircam_id']].values)
-print(np.unique(cla['which_nircam'].values))
+# print(np.unique(cla['which_nircam'].values))
 
-for i in range(100,200):
-    print(cat[['ID','zfit_50','zfit_16','zfit_84','logM_50','logM_16','logM_84','logMt_50','logMt_16','logMt_84']].values[i])
+# for i in range(100,200):
+#     print(cat[['ID','zfit_50','zfit_16','zfit_84','logM_50','logM_16','logM_84','logMt_50','logMt_16','logMt_84']].values[i])
